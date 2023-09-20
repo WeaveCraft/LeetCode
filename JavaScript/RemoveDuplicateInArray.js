d844a91948e66b1443b474ca08f6b54a96afae6c
Viktor Hurtig <Viktor.hurtig@outlook.com>
2023-09-20 19:07:29 +0200

    Remove duplicates in Array JavaScript.

---
+++ JavaScript/RemoveDuplicateInArray.js
@@ -0,0 +1,8 @@
+const removeDuplicates = (nums) => {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            nums.splice(i + 1, 1);
+            i--;
+        }
+    }
+};
